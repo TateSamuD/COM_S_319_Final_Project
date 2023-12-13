4c26240b1068b239b3ab0ea763e8bcb6be10175a
Tatenda Samudzi <tatenda.samudzi@gmail.com>
2023-12-13 06:49:34 -0600

    html update

--- basic_view/main_v2.js
+++ basic_view/main_v2.js
@@ -60,303 +60,191 @@ const jsonData = {
         y: -1,
         z: 0,
       },
-      timestamp: 1702323857.31349,
-    },
-    {
-      gyroscope: {
-        x: -2,
-        y: -14,
-        z: -15,
-      },
-      timestamp: 1702323859.3314,
-    },
-    {
-      gyroscope: {
-        x: 9,
-        y: -53,
-        z: 26,
-      },
-      timestamp: 1702323860.3402479,
-    },
-    {
-      gyroscope: {
-        x: 7,
-        y: -44,
-        z: 6,
-      },
-      timestamp: 1702323861.3596714,
-    },
-    {
-      gyroscope: {
-        x: 18,
-        y: 36,
-        z: -9,
-      },
-      timestamp: 1702323862.3807979,
-    },
-    {
-      gyroscope: {
-        x: -86,
-        y: 34,
-        z: 5,
-      },
-      timestamp: 1702323863.4000785,
-    },
-    {
-      gyroscope: {
-        x: -10,
-        y: 42,
-        z: -6,
-      },
-      timestamp: 1702323864.4214542,
-    },
-    {
-      gyroscope: {
-        x: 13,
-        y: 10,
-        z: 18,
-      },
-      timestamp: 1702323865.4430087,
-    },
-    {
-      gyroscope: {
-        x: -1,
-        y: -101,
-        z: 0,
-      },
-      timestamp: 1702323866.4650147,
-    },
-    {
-      gyroscope: {
-        x: 0,
-        y: -36,
-        z: -5,
-      },
-      timestamp: 1702323867.4907694,
-    },
-    {
-      gyroscope: {
-        x: 33,
-        y: 12,
-        z: -23,
-      },
-      timestamp: 1702323868.5128508,
-    },
-    {
-      gyroscope: {
-        x: 30,
-        y: 5,
-        z: 5,
-      },
-      timestamp: 1702323869.5349886,
-    },
-    {
-      gyroscope: {
-        x: -48,
-        y: -7,
-        z: 7,
-      },
-      timestamp: 1702323870.5569665,
-    },
-    {
-      gyroscope: {
-        x: -33,
-        y: 21,
-        z: 6,
-      },
-      timestamp: 1702323871.5791528,
-    },
-    {
-      gyroscope: {
-        x: -2,
-        y: -2,
-        z: 0,
-      },
-      timestamp: 1702323872.606039,
+      timestamp: 1702410092.9506986,
     },
     {
       gyroscope: {
         x: -4,
-        y: 1,
+        y: -1,
         z: 0,
       },
-      timestamp: 1702323873.6287296,
+      timestamp: 1702410093.9596395,
     },
     {
       gyroscope: {
-        x: 6,
-        y: 3,
+        x: -4,
+        y: 0,
         z: 2,
       },
-      timestamp: 1702323874.6515577,
+      timestamp: 1702410094.9685519,
     },
     {
       gyroscope: {
-        x: 35,
-        y: -3,
+        x: 10,
+        y: -35,
         z: 0,
       },
-      timestamp: 1702323875.6744723,
+      timestamp: 1702410095.977557,
     },
     {
       gyroscope: {
-        x: -13,
-        y: -46,
-        z: 27,
+        x: 26,
+        y: 18,
+        z: 9,
       },
-      timestamp: 1702323876.6974347,
+      timestamp: 1702410096.9969535,
     },
     {
       gyroscope: {
         x: 3,
-        y: -22,
-        z: 16,
+        y: 24,
+        z: 0,
       },
-      timestamp: 1702323877.7241735,
+      timestamp: 1702410098.0224729,
     },
     {
       gyroscope: {
-        x: -8,
+        x: -78,
+        y: -21,
+        z: -39,
+      },
+      timestamp: 1702410099.043644,
+    },
+    {
+      gyroscope: {
+        x: -10,
         y: 7,
-        z: -16,
+        z: 8,
       },
-      timestamp: 1702323878.747477,
+      timestamp: 1702410100.0649433,
     },
     {
       gyroscope: {
-        x: -4,
-        y: -2,
-        z: 3,
-      },
-      timestamp: 1702323879.7707362,
-    },
-    {
-      gyroscope: {
-        x: -5,
-        y: -1,
-        z: 0,
-      },
-      timestamp: 1702323880.7962222,
-    },
-    {
-      gyroscope: {
-        x: -4,
-        y: -2,
-        z: 0,
-      },
-      timestamp: 1702323881.8203578,
-    },
-    {
-      gyroscope: {
-        x: -4,
+        x: -1,
         y: 2,
-        z: -1,
+        z: -2,
       },
-      timestamp: 1702323882.8482444,
+      timestamp: 1702410101.0860002,
     },
     {
       gyroscope: {
-        x: -7,
-        y: 117,
-        z: -29,
+        x: -4,
+        y: 3,
+        z: 4,
       },
-      timestamp: 1702323883.871961,
+      timestamp: 1702410102.107987,
+    },
+    {
+      gyroscope: {
+        x: 25,
+        y: -25,
+        z: 6,
+      },
+      timestamp: 1702410103.133729,
     },
     {
       gyroscope: {
         x: -6,
-        y: 5,
-        z: -1,
+        y: -19,
+        z: -2,
       },
-      timestamp: 1702323884.8957312,
+      timestamp: 1702410104.155419,
     },
     {
       gyroscope: {
-        x: -25,
-        y: 16,
-        z: 4,
+        x: -28,
+        y: -25,
+        z: -20,
       },
-      timestamp: 1702323885.9201443,
-    },
-    {
-      gyroscope: {
-        x: -9,
-        y: -10,
-        z: -1,
-      },
-      timestamp: 1702323886.9441946,
-    },
-    {
-      gyroscope: {
-        x: -4,
-        y: -1,
-        z: 0,
-      },
-      timestamp: 1702323887.9721084,
-    },
-    {
-      gyroscope: {
-        x: -3,
-        y: 0,
-        z: 0,
-      },
-      timestamp: 1702323888.9963539,
-    },
-    {
-      gyroscope: {
-        x: -22,
-        y: -58,
-        z: 35,
-      },
-      timestamp: 1702323890.0206287,
+      timestamp: 1702410105.1772778,
     },
     {
       gyroscope: {
         x: 0,
-        y: -37,
-        z: 49,
+        y: -7,
+        z: 3,
       },
-      timestamp: 1702323891.0450125,
+      timestamp: 1702410106.199061,
     },
     {
       gyroscope: {
-        x: 46,
-        y: 126,
-        z: -101,
+        x: -1,
+        y: 20,
+        z: -2,
       },
-      timestamp: 1702323892.069324,
+      timestamp: 1702410107.220942,
     },
     {
       gyroscope: {
-        x: 4,
-        y: -180,
-        z: 71,
+        x: -11,
+        y: 25,
+        z: 6,
       },
-      timestamp: 1702323893.0859663,
+      timestamp: 1702410108.2473538,
     },
     {
       gyroscope: {
-        x: -32,
-        y: 170,
-        z: -85,
+        x: -7,
+        y: -3,
+        z: 0,
       },
-      timestamp: 1702323894.096619,
+      timestamp: 1702410109.2694798,
     },
     {
       gyroscope: {
-        x: -43,
-        y: -45,
-        z: -4,
+        x: -17,
+        y: -8,
+        z: 3,
       },
-      timestamp: 1702323895.107101,
+      timestamp: 1702410110.291801,
+    },
+    {
+      gyroscope: {
+        x: 27,
+        y: -28,
+        z: -2,
+      },
+      timestamp: 1702410111.314128,
+    },
+    {
+      gyroscope: {
+        x: -21,
+        y: -16,
+        z: -6,
+      },
+      timestamp: 1702410112.3368447,
+    },
+    {
+      gyroscope: {
+        x: 1,
+        y: -13,
+        z: -6,
+      },
+      timestamp: 1702410113.3631215,
+    },
+    {
+      gyroscope: {
+        x: -2,
+        y: -3,
+        z: -11,
+      },
+      timestamp: 1702410114.3860452,
+    },
+    {
+      gyroscope: {
+        x: 12,
+        y: -65,
+        z: -36,
+      },
+      timestamp: 1702410115.4090462,
     },
     {
       gyroscope: {
         x: -4,
-        y: 0,
+        y: -2,
         z: 0,
       },
-      timestamp: 1702323896.1176093,
+      timestamp: 1702410116.4321952,
     },
     {
       gyroscope: {
@@ -364,7 +252,15 @@ const jsonData = {
         y: -1,
         z: 0,
       },
-      timestamp: 1702323897.1282692,
+      timestamp: 1702410117.4555097,
+    },
+    {
+      gyroscope: {
+        x: -4,
+        y: -1,
+        z: 0,
+      },
+      timestamp: 1702410118.4833345,
     },
   ],
 };
@@ -380,12 +276,12 @@ async function animate() {
   for (let i = 0; i < jsonData.sensor_data.length; i++) {
     const latestData = jsonData.sensor_data[i].gyroscope;
 
-    currentRotation.x += latestData.x * 0.0001; // Adjust the scaling factor as needed
-    // currentRotation.x += 1 * 0.001;
-    currentRotation.y += latestData.y * 0.0001;
-    // currentRotation.y += 1 * 0.001;
-    currentRotation.z += latestData.z * 0.0001;
-    // currentRotation.z += 1 * 0.001;
+    // currentRotation.x += latestData.x * 0.0001; // Adjust the scaling factor as needed
+    // // currentRotation.x += 1 * 0.001;
+    // currentRotation.y += latestData.y * 0.0001;
+    // // currentRotation.y += 1 * 0.001;
+    // currentRotation.z += latestData.z * 0.0001;
+    // // currentRotation.z += 1 * 0.001;
 
     cube.rotation.x = currentRotation.x;
     cube.rotation.y = currentRotation.y;
